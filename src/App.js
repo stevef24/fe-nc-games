5a87773298e4ea904ab0f529c68c3e286e7f336f
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-03-13 16:55:20 +0000

    added error page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<Route path="/" element={<LandingPage />} />
 				<Route path="/home" element={<Home />} />
 				<Route path="/about" element={<About />} />
+				<Route path="/*" element={<ErrorPage />} />
 			</Routes>
 		</>
 	);
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const ErrorPage = () => {
-	return <div>ErrorPage</div>;
+	return (
+		<>
+			<Navbar />
+			<div className="flex items-center justify-center h-screen hero text-primary">
+				<div className="text-center hero-content">
+					<div className="max-w-lg">
+						<h1 className="mb-8 font-bold text-8xl">oops!</h1>
+						<p className="mb-8 text-5xl">404 page not found </p>
+						<NavLink to="/" className="btn btn-outline btn-secondary">
+							<FaHome className="mr-2" />
+							Back to home
+						</NavLink>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default ErrorPage;
