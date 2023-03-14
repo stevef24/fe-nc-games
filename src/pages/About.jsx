b5d82b6854502a5e868f8d9d488a41845e2d3fa4
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-03-14 09:51:19 +0000

    minor changes to the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,15 +17,21 @@ const About = () => {
 					/>
 					<section>
 						<h1 className="text-6xl font-bold ">About us</h1>
-						<p className="w-3/4 py-6 ">
-							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-							excepturi exercitationem quasi. In deleniti eaque aut repudiandae.
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-							ipsum similique a, in vel cupiditate? Aliquam labore provident
-							amet distinctio fugit tempora earum autem, sapiente id laudantium
-							necessitatibus eum, voluptates illo dolorem ex nihil blanditiis
-							sunt fugiat adipisci nisi eaque. Quasi tempore aperiam velit odit
-							sint doloribus similique minus laborum. et a id nisi.
+						<p className="w-5/6 py-6 ">
+							Welcome to our online platform dedicated to providing game reviews
+							and facilitating user discussions. Our team of experienced
+							reviewers is committed to bringing you the latest insights and
+							critiques on the most popular games on the market. From triple-A
+							titles to indie gems, we cover it all. But we don't stop there. We
+							believe that gaming is an interactive experience, and that's why
+							we've created a community-driven forum where users can discuss
+							their favorite games, share their opinions, and connect with other
+							gamers from around the world. Our forum is a safe and inclusive
+							space for gamers of all ages, genders, and backgrounds to come
+							together and discuss their passion for gaming. At our core, we are
+							gamers just like you. We understand the importance of a reliable
+							review and the value of connecting with other gamers who share our
+							interests.
 						</p>
 						<NavLink to="/home" className="btn btn-primary">
 							Search reviews
